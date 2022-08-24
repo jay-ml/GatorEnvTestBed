15147fc948eaea05d401ad7f1f2c260cd432c459
Jay Luther <49818405+jay-ml@users.noreply.github.com>
2022-08-24 12:32:45 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,18 @@
 gatorEnvironment.beginEnvironment()
 basic.forever(function () {
-    serial.writeLine("Temp: ");
-    serial.writeNumber(gatorEnvironment.getMeasurement(measurementType.degreesC));
+    serial.writeLine("Temp: ")
+    serial.writeNumber(gatorEnvironment.getMeasurement(measurementType.degreesC))
     serial.writeLine("")
-
-    serial.writeLine("Humidity: ");
-    serial.writeNumber(gatorEnvironment.getMeasurement(measurementType.humidity));
+    serial.writeLine("Humidity: ")
+    serial.writeNumber(gatorEnvironment.getMeasurement(measurementType.humidity))
     serial.writeLine("")
-
-    serial.writeLine("Pressure: ");
-    serial.writeNumber(gatorEnvironment.getMeasurement(measurementType.pressure));
+    serial.writeLine("Pressure: ")
+    serial.writeNumber(gatorEnvironment.getMeasurement(measurementType.pressure))
     serial.writeLine("")
-
-    serial.writeLine("eCO2: ");
-    serial.writeNumber(gatorEnvironment.getMeasurement(measurementType.eCO2));
+    serial.writeLine("eCO2: ")
+    serial.writeNumber(gatorEnvironment.getMeasurement(measurementType.eCO2))
     serial.writeLine("")
-
-    serial.writeLine("TVOC: ");
-    serial.writeNumber(gatorEnvironment.getMeasurement(measurementType.TVOC));
+    serial.writeLine("TVOC: ")
+    serial.writeNumber(gatorEnvironment.getMeasurement(measurementType.TVOC))
     serial.writeLine("")
-	
 })
