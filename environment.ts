1ee0507eac39b0e6cfd34a03382628f571e7a1a9
Jay Luther <49818405+jay-ml@users.noreply.github.com>
2022-08-25 16:47:34 -0600

    Update environment.ts

--- environment.ts
+++ environment.ts
@@ -1,4 +1,4 @@
-const BME280_ADDRESS = 0xEE;
+const BME280_ADDRESS = 0x77;
 const BME280_DIG_T1_LSB_REG = 0x88;
 const BME280_DIG_T1_MSB_REG = 0x89;
 const BME280_DIG_T2_LSB_REG = 0x8A;
@@ -77,6 +77,19 @@ class Environment {
     pressure: number = 0;
     humidity: number = 0;
 
+    t_fine: number;
+
+    calibration = new SensorCalibration();
+    BMErunMode: number;
+    BMEtStandby: number;
+    BMEfilter: number;
+    BMEtempOverSample: number;
+    BMEpressOverSample: number;
+    BMEhumidOverSample: number;
+    BMEtempCorrection: number;
+
+    _referencePressure = 101325.0; //Default but is changeable
+
     constructor(){
         return;
     }
@@ -113,8 +126,6 @@ class Environment {
         pins.i2cWriteNumber(0x5B, 0xF4, NumberFormat.UInt8LE, false)
         serial.writeLine("CCS811 Started")
 
-        pause(100)
-
         //Set Drive Mode - modified from sparkfun
         pins.i2cWriteNumber(0x5B, 0x01, NumberFormat.UInt8LE, false)
         let current_mode = pins.i2cReadNumber(0x5B, NumberFormat.UInt8LE, false)
@@ -127,44 +138,182 @@ class Environment {
         serial.writeNumber(current_mode)
         serial.writeLine("")
 
+        // CCS needs 4 seconds before data is available, add pause as needed 
+        // after BME init
+
+        //BME init
+        this.calibration.dig_T1 = this.getUInt16LE(0x88)
+        this.calibration.dig_T2 = this.getInt16LE(0x8A)
+        this.calibration.dig_T3 = this.getInt16LE(0x8C)
+        this.calibration.dig_P1 = this.getUInt16LE(0x8E)
+        this.calibration.dig_P2 = this.getInt16LE(0x90)
+        this.calibration.dig_P3 = this.getInt16LE(0x92)
+        this.calibration.dig_P4 = this.getInt16LE(0x94)
+        this.calibration.dig_P5 = this.getInt16LE(0x96)
+        this.calibration.dig_P6 = this.getInt16LE(0x98)
+        this.calibration.dig_P7 = this.getInt16LE(0x9A)
+        this.calibration.dig_P8 = this.getInt16LE(0x9C)
+        this.calibration.dig_P9 = this.getInt16LE(0x9E)
+        this.calibration.dig_H1 = this.getreg(0xA1)
+        this.calibration.dig_H2 = this.getInt16LE(0xE1)
+        this.calibration.dig_H3 = this.getreg(0xE3)
+        let a = this.getreg(0xE5)
+        this.calibration.dig_H4 = (this.getreg(0xE4) << 4) + (a % 16)
+        this.calibration.dig_H5 = (this.getreg(0xE6) << 4) + (a >> 4)
+        this.calibration.dig_H6 = this.getInt8LE(0xE7)
+        this.setreg(0xF2, 0x04)
+        this.setreg(0xF4, 0x2F)
+        this.setreg(0xF5, 0x0C)
+
+    }
+
+    setreg(reg: number, dat: number): void {
+        let buf = pins.createBuffer(2);
+        buf[0] = reg;
+        buf[1] = dat;
+        pins.i2cWriteBuffer(BME280_ADDRESS, buf);
+    }
+
+    getreg(reg: number): number {
+        pins.i2cWriteNumber(BME280_ADDRESS, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BME280_ADDRESS, NumberFormat.UInt8BE);
+    }
+
+    getInt8LE(reg: number): number {
+        pins.i2cWriteNumber(BME280_ADDRESS, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BME280_ADDRESS, NumberFormat.Int8LE);
+    }
+
+    getUInt16LE(reg: number): number {
+        pins.i2cWriteNumber(BME280_ADDRESS, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BME280_ADDRESS, NumberFormat.UInt16LE);
+    }
+
+    getInt16LE(reg: number): number {
+        pins.i2cWriteNumber(BME280_ADDRESS, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BME280_ADDRESS, NumberFormat.Int16LE);
+    }
+
+    get(): void {
+        let adc_T = (this.getreg(0xFA) << 12) + (this.getreg(0xFB) << 4) + (this.getreg(0xFC) >> 4)
+        let var1 = (((adc_T >> 3) - (this.calibration.dig_T1 << 1)) * this.calibration.dig_T2) >> 11
+        let var2 = (((((adc_T >> 4) - this.calibration.dig_T1) * ((adc_T >> 4) - this.calibration.dig_T1)) >> 12) * this.calibration.dig_T3) >> 14
+        let t = var1 + var2
+        this.temperature = Math.idiv((t * 5 + 128) >> 8, 100)
+        var1 = (t >> 1) - 64000
+        var2 = (((var1 >> 2) * (var1 >> 2)) >> 11) * this.calibration.dig_P6
+        var2 = var2 + ((var1 * this.calibration.dig_P5) << 1)
+        var2 = (var2 >> 2) + (this.calibration.dig_P4 << 16)
+        var1 = (((this.calibration.dig_P3 * ((var1 >> 2) * (var1 >> 2)) >> 13) >> 3) + (((this.calibration.dig_P2) * var1) >> 1)) >> 18
+        var1 = ((32768 + var1) * this.calibration.dig_P1) >> 15
+        if (var1 == 0)
+            return; // avoid exception caused by division by zero
+        let adc_P = (this.getreg(0xF7) << 12) + (this.getreg(0xF8) << 4) + (this.getreg(0xF9) >> 4)
+        let _p = ((1048576 - adc_P) - (var2 >> 12)) * 3125
+        _p = Math.idiv(_p, var1) * 2;
+        var1 = (this.calibration.dig_P9 * (((_p >> 3) * (_p >> 3)) >> 13)) >> 12
+        var2 = (((_p >> 2)) * this.calibration.dig_P8) >> 13
+        this.pressure = _p + ((var1 + var2 + this.calibration.dig_P7) >> 4)
+        let adc_H = (this.getreg(0xFD) << 8) + this.getreg(0xFE)
+        var1 = t - 76800
+        var2 = (((adc_H << 14) - (this.calibration.dig_H4 << 20) - (this.calibration.dig_H5 * var1)) + 16384) >> 15
+        var1 = var2 * (((((((var1 * this.calibration.dig_H6) >> 10) * (((var1 * this.calibration.dig_H3) >> 11) + 32768)) >> 10) + 2097152) * this.calibration.dig_H2 + 8192) >> 14)
+        var2 = var1 - (((((var1 >> 15) * (var1 >> 15)) >> 7) * this.calibration.dig_H1) >> 4)
+        if (var2 < 0) var2 = 0
+        if (var2 > 419430400) var2 = 419430400
+        this.humidity = (var2 >> 12) >> 10
+    }
+
+
+// --------
+
+    dataAvailable() {
+        //Check if Data is available
+        pins.i2cWriteNumber(0x5B, 0x00, NumberFormat.UInt8LE, false)
+        let status = pins.i2cReadNumber(0x5B, NumberFormat.UInt8LE, false)
+
+        return (status & 1 << 3)
+    }
+
+    readAlgorithmResults() {
+        serial.writeLine("Data Available");
+        let address = 0x5B;
+        let offset = 0x02;
+        let data: number[] = [];
+        pins.i2cWriteNumber(address, offset, NumberFormat.UInt8LE, false);
+        this.CO2 = pins.i2cReadNumber(0x5B, NumberFormat.UInt16BE, true)
+        this.tVOC = pins.i2cReadNumber(0x5B, NumberFormat.UInt16BE, false)
+
+    }
+
+    getCO2() {
+        if (this.dataAvailable()){
+            this.readAlgorithmResults();
+            return this.CO2;
+        } else {
+            return 0;
+        }
+
+    }
+
+    getTVOC() {
+        if (this.dataAvailable()) {
+            this.readAlgorithmResults();
+            return this.tVOC;
+        } else {
+            return 0;
+        }
     }
 
     readTempC(){
+        this.get();
         return this.temperature;
     }
 
     readTempF(){
-        return 0;
+        this.get();
+        return 32 + Math.idiv(this.temperature * 9, 5)
     }
 
 
     readFloatHumidity(){
+        this.get();
         return this.humidity;
     }
 
     readFloatPressure(){
-        return this.pressure;
+        this.get();
+        return Math.idiv(this.pressure, 100);
     }
 
-    dataAvailable(){
-        return false;
+}
 
+
+
+class SensorCalibration {
+
+    dig_T1: number;
+    dig_T2: number;
+    dig_T3: number;
+
+    dig_P1: number;
+    dig_P2: number;
+    dig_P3: number;
+    dig_P4: number;
+    dig_P5: number;
+    dig_P6: number;
+    dig_P7: number;
+    dig_P8: number;
+    dig_P9: number;
+
+    dig_H1: number;
+    dig_H2: number;
+    dig_H3: number;
+    dig_H4: number;
+    dig_H5: number;
+    dig_H6: number;
+
+    constructor() {
+        return;
     }
-
-    readAlgorithmResults(){
-        return 0;
-
-    }
-
-    getCO2(){
-        return this.CO2;
-
-    }
-
-    getTVOC(){
-        return this.tVOC;
-
-    }
-
-
 }
